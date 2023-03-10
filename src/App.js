47b4602eba126168b381cff4c4d5f17978636716
udilpedia <ffaizdaffa@gmail.com>
2023-03-10 15:00:16 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 
 
 function App() {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import iconhuman from '../assets/iconhuman.png';
+import { HiOutlineArrowNarrowRight } from "react-icons/md";
+
+
+const Home = () => {
+    return (
+       <div>
+        
+
+            <div>
+                <div>
+                    <h2>
+                        Saya seorang UI/UX Desainer (Magang)
+                    </h2>
+                    <p>
+                        Saya mempunyai 6 bulan pengalaman dalam dunia desain UI/UX. 
+                        Saat ini, saya bekerja di sebuah perusahaan sebagai freelancer 
+                        dan saya juga suka dalam tantangan dalam dunia desain. 
+                    </p>
+                </div>
+            </div>
+       </div>
+    );
+  
+};
+
+export default Home;
